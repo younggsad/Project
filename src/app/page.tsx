803d6de9380cb763c6e0151da330c4fd875b234b
younggsad <vel007evgeniy@gmail.com>
2025-11-12 16:45:19 +0300

    Начальная версия

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from "@/app/Home.module.css";
 export default function HomePage() {
   return (
     <div className={styles.homePage}>
-      <h1>Hello</h1>
+      
     </div>
   );
 }
